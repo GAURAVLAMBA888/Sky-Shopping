524035f2fe0e2994c7f46c7540e6a19354636e31
GAURAV <gauravlamba888@gmail.com>
2023-06-18 15:31:31 +0530

    Completed FrontEnd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
-import "./App.css";
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+      <Home />
   );
 }
 
